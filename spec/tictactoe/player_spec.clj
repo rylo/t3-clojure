425df5e82c0568d346ffbce5af5b8b3b5a432eaf
Rylan Dirksen <rylandirksen@gmail.com>
2013-01-08 11:28:43 -0600

    Created player_spec

---
+++ spec/tictactoe/player_spec.clj
@@ -0,0 +1,13 @@
+(ns tictactoe.core-spec
+  (:require [speclj.core :refer :all]
+            [tictactoe.player :refer :all]))
+
+; (describe "Player"
+
+  ; (it "should return a number"
+  ;   (should (= (tictactoe.player/number) 2)))
+  ; 
+  ; (it "is true"
+  ;   (should (= (tictactoe.player/number) 1))))
+
+(run-specs)
