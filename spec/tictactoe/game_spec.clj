ce6385aef73466938030e690087a4a307868f464
Rylan Dirksen <rylandirksen@gmail.com>
2013-01-10 10:00:14 -0600

    Added valid-move? and get-human-move functions to game namespace

--- spec/tictactoe/game_spec.clj
+++ spec/tictactoe/game_spec.clj
@@ -9,6 +9,20 @@
 			(it "should return false if the game isn't over" (should= false (game-over? taken-row-present? empty-board row-vector)))
 			(it "should return false if the game isn't over" (should= true (game-over? taken-row-present? full-board row-vector)))))
 
+	(context "valid-move?"
+		(let [board (make-board 9 nil)]
+			(it "should return false on invalid moves" (should= false (valid-move? board "asdf")))
+			(it "should return false on invalid moves" (should= false (valid-move? board "12222")))
+			(it "should return true on valid moves" (should= true (valid-move? board "1")))))
+	
+	(context "get-human-move"
+		(it "should return the user's move if it is valid"
+			(let [board (make-board 9 nil)]
+				(should= "1" (with-redefs [read-line (constantly "1") println (constantly "")] (get-human-move board)))))
+		(it "should prompt the user for another move if it is invalid" (pending "ability to test recursive input")))
+	
+	
+	
 	; (context "start"
 	; 	(let [board (make-board 9 nil)]
 	; 		(it "should begin a game loop" (should= true (start (make-board 9 nil))))))
--- src/tictactoe/game.clj
+++ src/tictactoe/game.clj
@@ -1,3 +1,16 @@
-(ns tictactoe.game)
+(ns tictactoe.game
+	(:require [tictactoe.board :refer [get-marker]]
+			  [tictactoe.io :refer [prompt]]))
 
-(defn game-over? [function board row-vector] (function board row-vector))
+(defn game-over? [function board row-vector]
+	(function board row-vector))
+
+(defn valid-move? [board move-destination]
+	(nil? (try (get-marker board (Integer. move-destination)) (catch Exception e false) (finally true))))
+
+(defn get-human-move [board]
+	(loop []
+		(let [prompt (prompt "Please make a move:") valid (valid-move? board prompt)]
+		(if (true? valid)
+			prompt
+			(recur)))))
