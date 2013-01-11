72784d35f573a85d816d255827c65a7460d14c36
Rylan Dirksen <rylandirksen@gmail.com>
2013-01-11 01:12:54 -0600

    Added :main definition to project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tictactoe "0.1.0-SNAPSHOT"
   :description "Tic Tac Toe in Clojure"
-  :url "http://example.com/FIXME"
+  :main tictactoe.runner
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[speclj "2.5.0"]
