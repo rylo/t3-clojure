5065abacf4f9e646a8ef36073508c3121ce4de83
Rylan Dirksen <rylandirksen@gmail.com>
2013-01-14 10:17:13 -0600

    Refactor and bug fixes

--- src/tictactoe/game.clj
+++ src/tictactoe/game.clj
@@ -1,14 +1,14 @@
 (ns tictactoe.game
 	(:require [tictactoe.player :refer :all]
-			  [tictactoe.board :refer [printable-board make-board get-marker taken-row-present? generate-rows set-marker]]
+			  [tictactoe.board :refer [printable-board make-board get-marker winning-row-present? generate-rows set-marker]]
 			  [tictactoe.io :refer [prompt set-output]]
 			  [tictactoe.game_rules :refer [game-over? valid-move?]]))
 			
-(defn alternate-players [player-list current-player] (if (= "x" current-player) (nth player-list 0) (nth player-list 1)))
+(defn alternate-players [player-list current-player] (if (= "x" (marker current-player)) (nth player-list 1) (nth player-list 0)))
 
 (defn start [board player-list]
-	(set-output (printable-board board))
 	(loop [current-player (first player-list) board board]	
+		(set-output (printable-board board))
 		(let [altered-board (set-marker board (:marker current-player) (get-move current-player board))]
 			(if (game-over? altered-board)
 				"game over"
