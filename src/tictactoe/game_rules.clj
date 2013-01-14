336e810b9ce122172c7591589191fdef19210c7f
Rylan Dirksen <rylandirksen@gmail.com>
2013-01-14 10:17:35 -0600

    Refactor and tie conditions added

--- src/tictactoe/game_rules.clj
+++ src/tictactoe/game_rules.clj
@@ -1,8 +1,11 @@
 (ns tictactoe.game_rules
-	(:require [tictactoe.board :refer [get-marker taken-row-present? generate-rows set-marker]]))
+	(:require [tictactoe.board :refer [get-marker winning-row-present? generate-rows set-marker full?]]))
 	
 (defn game-over? [board]
-	(taken-row-present? board (generate-rows)))
+	(winning-row-present? board (generate-rows)))
 
 (defn valid-move? [board move-destination]
 	(nil? (try (get-marker board (Integer. move-destination)) (catch Exception e false) (finally true))))
+	
+(defn game-over-with-tie? [board] 
+	(and (full? board) (not (winning-row-present? board (generate-rows)))))
