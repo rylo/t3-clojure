eed25555ce0a55143a98ecd5e5ff19919c201cd5
Rylan Dirksen <rylandirksen@gmail.com>
2013-01-14 10:16:49 -0600

    Added game-over-with-tie? board test

--- spec/tictactoe/game_rules_spec.clj
+++ spec/tictactoe/game_rules_spec.clj
@@ -1,4 +1,4 @@
-(ns tictactoe.io-spec
+(ns tictactoe.game-rules-spec
   (:require [speclj.core :refer :all]
 			[tictactoe.board :refer :all]
             [tictactoe.game_rules :refer :all]))
@@ -14,6 +14,18 @@
 			(it "should return false on invalid moves" (should= false (valid-move? board "asdf")))
 			(it "should return false on invalid moves" (should= false (valid-move? board "12222")))
 			(it "should return true on valid moves" (should= true (valid-move? board "1")))))
+			
+	(context "game-over-with-tie?"
+		(let [tied-board ["x" "o" "x" "o" "x" "o" "o" "x" "o"]
+			  not-tied-board ["x" "x" "x" "o" "x" "o" "o" "x" "o"]]
+			(it "should return true if the board is full and nobody has won"
+				(should= true (game-over-with-tie? tied-board)))
+			(it "should return true if the board is full and somebody has won"
+				(should= true (winning-row-present? not-tied-board (generate-rows)))
+				(should= true (full? not-tied-board))
+				(should= false (game-over-with-tie? not-tied-board)))
+		)
+	)
 )
 
 (run-specs)
