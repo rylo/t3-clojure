4792274f564b4563a1b2a075f1275aebd79a2af2
Rylan Dirksen <rylandirksen@gmail.com>
2013-01-21 11:07:33 -0600

    Added get-move multimethod and get-score function

--- spec/tictactoe/player_spec.clj
+++ spec/tictactoe/player_spec.clj
@@ -1,11 +1,16 @@
 (ns tictactoe.player-spec
   (:require [speclj.core :refer :all]
             [tictactoe.player :refer :all]
+			[tictactoe.game_rules :refer :all]
 			[tictactoe.board :refer :all]))
 
 (describe "Player"
 	(def human-player (tictactoe.player.Human. "x"))
 	(def easy-computer-player (tictactoe.player.EasyComputer. "x"))
+	(def ultimate-computer-player (tictactoe.player.UltimateComputer. "x"))
+	
+	(defn contains-value? [value collection]
+		(not (nil? (some #{value} collection))))
 	
 	(it "has a marker"
 		(should= "x" (:marker human-player)))
@@ -17,11 +22,25 @@
 					(should= 1 (with-redefs [read-line (constantly "1") println (constantly "")] (get-move human-player board)))))
 			(it "should prompt the user for another move if it is invalid" (pending "ability to test recursive input"))))
 	
-	(context "Dumb Computer Player"
+	(context "Easy Computer Player (random spots)"
 		(context "get-move"
 			(it "should return a move in an empty space"
 				(let [board ["x" "x" "x" "x" "x" "x" "x" "x" nil]] (should= 8 (get-move easy-computer-player board))))))
-	
+
+	(context "Ulimate Computer Player (minimax algorithm)"
+		(context "get-score"
+			(it "should return a number score based on the state of the board"
+				(let [board ["x" "x" "x" "x" "x" "x" "x" "x" "x"]] (should= 100 (get-score "x" board)))
+				(let [board ["x" "x" "x" "x" "x" "x" "x" "x" "x"]] (should= -100 (get-score "o" board)))
+				(let [board ["o" "o" "o" "o" "o" "o" "o" "o" "o"]] (should= -100 (get-score "x" board)))
+				(let [board ["x" nil nil nil "o" nil nil nil "o"]] (should= 0 (get-score "o" board)))
+				(let [board ["x" "x" "x" nil "o" nil nil nil "o"]] (should= -100 (get-score "o" board)))))
+		
+		(context "get-move"
+			(it "should return a move in an empty space"
+				(let [board ["x" "x" "x" "x" "x" "x" "x" "x" nil]] (should= 8 (get-move ultimate-computer-player board)))
+				(let [board ["o" "o" "x" "x" "o" "x" nil nil "o"]] (should (contains-value? (get-move ultimate-computer-player board) [6 7])))))
+	)
 )
 
 (run-specs)
--- src/tictactoe/player.clj
+++ src/tictactoe/player.clj
@@ -1,23 +1,44 @@
 (ns tictactoe.player
 	(:require [tictactoe.io :refer [prompt]]
-			  [tictactoe.game_rules :refer [empty-spaces valid-move?]]))
+			  [tictactoe.game_rules :refer [empty-spaces valid-move? get-winner game-over-with-tie? game-over?]]))
 
 (defprotocol Player
 	(marker [this])
 	(get-move [this board]))
 
+(defmulti get-move (fn [this board] (str (type this))))
+
+(defmethod get-move "class tictactoe.player.Human" [this board] 
+	(loop [prompt-message "Please make a move:"]
+		(let [prompt-return-value (prompt prompt-message) valid (valid-move? board prompt-return-value)]
+				(if valid
+					(Integer. prompt-return-value)
+					(recur "Invalid move, please try again.")))))
+
+(defmethod get-move "class tictactoe.player.EasyComputer" [this board] 
+	(rand-nth (empty-spaces board)))
+	
+(defmethod get-move "class tictactoe.player.UltimateComputer" [this board] 
+	(rand-nth (empty-spaces board)))
+
 (defrecord Human [marker]
 	Player
 	(marker [this] (:marker this))
-	(get-move [this board]
-		(loop [prompt-message "Please make a move:"]
-			(let [prompt-return-value (prompt prompt-message) valid (valid-move? board prompt-return-value)]
-					(if valid
-						(Integer. prompt-return-value)
-						(recur "Invalid move, please try again."))))))
-						
+	(get-move [this board]))
+
 (defrecord EasyComputer [marker]
 	Player
 	(marker [this] (:marker this))
-	(get-move [this board]
-		(rand-nth (empty-spaces board))))
+	(get-move [this board]))
+
+(defrecord UltimateComputer [marker]
+	Player
+	(marker [this] (:marker this))
+	(get-move [this board]))
+	
+(defn get-score [player-marker board]
+	(if (or (game-over-with-tie? board) (not (game-over? board)))
+		0
+		(if (= player-marker (get-winner board))
+			 100
+			-100)))
