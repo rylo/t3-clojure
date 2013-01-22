e14c6f6fbbf108a7aa3c3baf03fcfc40e8f61dfa
Rylan Dirksen <rylandirksen@gmail.com>
2013-01-22 14:57:12 -0600

    Fixed references to game-won?

--- src/tictactoe/game.clj
+++ src/tictactoe/game.clj
@@ -2,7 +2,7 @@
 	(:require [tictactoe.player :refer :all]
 			  [tictactoe.board :refer [printable-board make-board get-marker winning-row-present? generate-rows set-marker]]
 			  [tictactoe.io :refer [prompt set-output]]
-			  [tictactoe.game_rules :refer [get-winner game-over? game-over-with-tie? valid-move?]]))
+			  [tictactoe.game_rules :refer [get-winner game-won? game-over-with-tie? valid-move?]]))
 			
 (defn alternate-players [player-list current-player] 
 	(if (= (nth player-list 0) current-player)
@@ -18,7 +18,7 @@
 	(loop [current-player (first player-list) board board]	
 		(set-output (printable-board board))
 		(let [altered-board (set-marker board (:marker current-player) (get-move current-player board))]
-			(if (or (game-over? altered-board) (game-over-with-tie? altered-board))
+			(if (or (game-won? altered-board) (game-over-with-tie? altered-board))
 				(do (set-output (get-ending-message altered-board)) (set-output (printable-board altered-board)))
 				(recur (alternate-players player-list current-player) altered-board)))))
 
