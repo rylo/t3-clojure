93aa9a6451510d8026a02773893b7d6c173eb8a7
Rylan Dirksen <rylandirksen@gmail.com>
2013-01-28 18:23:28 -0600

    Finished depth pruning and tweaked algorithm to take immediate wins

--- spec/tictactoe/negamax_spec.clj
+++ spec/tictactoe/negamax_spec.clj
@@ -6,7 +6,10 @@
 (context "UltimateComputer negamax"
 	(context "score-empty-spaces"
 		(it "should return an array of best moves"
-			(should= [50 100 50 50 100] (score-empty-spaces "x" ["x" "o" nil nil nil nil "x" "x" nil]))))
+			(should= [50 100 50 50 100] (score-empty-spaces "x" ["x" "o" nil nil nil nil "x" "x" nil]))
+			(should= [50 50 50 100] (score-empty-spaces "o" ["x" nil "x" nil "x" nil "o" nil "o"]))
+		)
+	)
 			
 	(context "get-best-move"
 		(it "should return the space index of the optimal move"
@@ -14,7 +17,9 @@
 			(should= 3 (get-best-move "x" ["x" "o" nil nil nil nil "x" nil nil]))
 			(should= 2 (get-best-move "o" ["x" "x" nil nil nil nil "o" nil nil]))
 			(should= 6 (get-best-move "o" [nil nil "x" nil "x" nil nil nil "o"]))
-			(should= 4 (get-best-move "o" ["x" nil nil nil nil nil nil nil nil]))))
+			(should= 7 (get-best-move "o" ["x" nil "x" nil "x" nil "o" nil "o"]))
+			(should= 4 (get-best-move "o" ["x" nil nil nil nil nil nil nil nil]))
+			))
 
 	(context "get-score"
 		(it "should return a number score based on the state of the board"
--- src/tictactoe/negamax.clj
+++ src/tictactoe/negamax.clj
@@ -9,23 +9,25 @@
 		"x"))
 
 (defn get-score [player-marker board]
-	(if (game-over-with-tie? board)
-		0
-		(if (= player-marker (get-winner board))
-			 100
-			-100)))
+	(cond
+		(= player-marker (get-winner board)) 100
+		(= (alternate-player-marker player-marker) (get-winner board)) -100
+		(winning-move-available? player-marker board (generate-winning-combinations)) 50
+		(or (not (game-over? board)) (game-over-with-tie? board)) 0))
 
 (defn negamax [board player-marker depth]
 	(for [empty-space (empty-spaces board)
 		:let[altered-board (set-marker board player-marker empty-space)]]
-			(if (or (game-over? altered-board) (= depth 5))
+			(if (or (game-over? altered-board) (= depth 4))
 				(int (/ (get-score player-marker altered-board) depth))
 				(* -1 (apply max (flatten (negamax altered-board (alternate-player-marker player-marker) (inc depth))))))))
 
 (defn score-empty-spaces [player-marker board]
 	(for [empty-space (empty-spaces board)
 		  :let [altered-board (set-marker board player-marker empty-space)]]
-				(* -1 (apply max (flatten (negamax altered-board (alternate-player-marker player-marker) 1))))))
+				(if (winning-move-available? player-marker board (generate-winning-combinations)) 
+					(get-score player-marker altered-board)
+					(* -1 (apply max (flatten (negamax altered-board (alternate-player-marker player-marker) 1)))))))
 		
 (defn get-best-move [player-marker board] 
 	(key (apply max-key val (apply hash-map (interleave (empty-spaces board) (score-empty-spaces player-marker board))))))
