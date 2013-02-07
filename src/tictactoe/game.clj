f46f058cbb4f8b748004cd303f798471038c9e03
Rylan Dirksen <rylandirksen@gmail.com>
2013-02-07 13:03:22 -0600

    game-setup namespace integration

--- src/tictactoe/game.clj
+++ src/tictactoe/game.clj
@@ -1,8 +1,9 @@
 (ns tictactoe.game
-	(:require [tictactoe.player :refer :all]
-			  [tictactoe.board :refer [make-board get-marker generate-winning-combinations set-marker]]
-			  [tictactoe.io :refer [prompt print-output printable-board]]
-			  [tictactoe.game_rules :refer [get-winner winning-row-present? game-over-with-tie? valid-move?]]))
+	(:require [tictactoe.player :refer [get-move]]
+			  [tictactoe.board :refer [set-marker]]
+			  [tictactoe.io :refer [print-output printable-board]]
+			  [tictactoe.game-rules :refer [get-winner winning-row-present? game-over-with-tie? valid-move?]]
+			  [tictactoe.game-setup :refer [build-new-game]]))
 			
 (defn alternate-players [player-list current-player] 
 	(if (= (nth player-list 0) current-player)
@@ -14,15 +15,15 @@
 		"It's a tie!"
 		(format "Player %s wins!" (get-winner board))))
 
-(defn start [board player-list]
-	(loop [current-player (first player-list) board board]	
-		(print-output (printable-board board))
-		(let [altered-board (set-marker board (:marker current-player) (get-move current-player board))]
-			(if (or (winning-row-present? altered-board) (game-over-with-tie? altered-board))
-				(do (print-output (get-ending-message altered-board)) (print-output (printable-board altered-board)))
-				(recur (alternate-players player-list current-player) altered-board)))))
+(defn start [new-game]
+	(let [board (first new-game) player-list (last new-game)]
+		(loop [current-player (first player-list) board board]	
+			(print-output (printable-board board))
+			(let [altered-board (set-marker board (:marker current-player) (get-move current-player board))]
+				(if (or (winning-row-present? altered-board) (game-over-with-tie? altered-board))
+					(do (print-output (get-ending-message altered-board)) (print-output (printable-board altered-board)))
+					(recur (alternate-players player-list current-player) altered-board))))))
 
 (defn set-up-new-game []
-	(let [board (make-board 9 nil) player-list [(tictactoe.player.Human. "x") (tictactoe.player.UltimateComputer. "o")]]
-		(print-output "Welcome to Tic Tac Toe!")
-		(start board player-list)))
+	(print-output "Welcome to Tic Tac Toe!")
+		(start (build-new-game)))
