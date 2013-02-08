3cc6b7f0e178fd4de2c2fd5f51ddf74aaef36201
Rylan Dirksen <rylandirksen@gmail.com>
2013-02-08 11:05:17 -0600

    Can now select first player

--- spec/tictactoe/game_setup_spec.clj
+++ spec/tictactoe/game_setup_spec.clj
@@ -4,26 +4,35 @@
             [tictactoe.game :refer :all]))
 
 (describe "game_setup"
-	; (around [nothing] (with-redefs [println (constantly "")])) ; why does this make all tests always pass?
 
-	(context "build-new-game"
+	(describe "build-new-game"
 		(it "should return a board and a list with two computer players"
 			(should= [[nil nil nil nil nil nil nil nil nil] [(tictactoe.player.UltimateComputer. "x") (tictactoe.player.UltimateComputer. "x")]] 
-				(with-redefs [get-computer-difficulty (constantly (tictactoe.player.UltimateComputer. "x"))] 
+				(with-redefs [get-computer-difficulty (constantly (tictactoe.player.UltimateComputer. "x"))
+							  set-first-player (constantly [(tictactoe.player.UltimateComputer. "x") (tictactoe.player.UltimateComputer. "x")])] 
 					(with-in-str "0" (build-new-game)))))
 		(it "should return a board and a list of one human and one computer player"
 			(should= [[nil nil nil nil nil nil nil nil nil] [(tictactoe.player.Human. "x") (tictactoe.player.UltimateComputer. "x")]] 
-				(with-redefs [get-computer-difficulty (constantly (tictactoe.player.UltimateComputer. "x"))] 
+				(with-redefs [get-computer-difficulty (constantly (tictactoe.player.UltimateComputer. "x"))
+							  set-first-player (constantly [(tictactoe.player.Human. "x") (tictactoe.player.UltimateComputer. "x")])] 
 					(with-in-str "1" (build-new-game)))))
 		(it "should return a board and a list with two human players"
 			(should= [[nil nil nil nil nil nil nil nil nil] [(tictactoe.player.Human. "x") (tictactoe.player.Human. "o")]] 
-				(with-redefs [get-computer-difficulty (constantly (tictactoe.player.UltimateComputer. "x"))] 
+				(with-redefs [get-computer-difficulty (constantly (tictactoe.player.UltimateComputer. "x"))
+							  set-first-player (constantly [(tictactoe.player.Human. "x") (tictactoe.player.Human. "o")])] 
 					(with-in-str "2" (build-new-game))))))
 	
-	(context "get-computer-difficulty"
-		(it "should set the computer player difficulty to easy" 
-			(should= (tictactoe.player.EasyComputer. "o") (with-in-str "1" (get-computer-difficulty "o"))))
-		(it "should set the computer player difficulty to easy" 
-			(should= (tictactoe.player.UltimateComputer. "o") (with-in-str "2" (get-computer-difficulty "o"))))))
-
+	(describe "get-computer-difficulty"
+		(with-redefs [println (constantly "")]
+			(it "should set the computer player difficulty to easy" 
+				(should= (tictactoe.player.EasyComputer. "o") (with-in-str "1" (get-computer-difficulty "o"))))
+			(it "should set the computer player difficulty to ultimate" 
+				(should= (tictactoe.player.UltimateComputer. "o") (with-in-str "2" (get-computer-difficulty "o")))))))
+			
+	(describe "set-first-player"
+		(it "should rearrange the player list so the selected player comes first"
+			(let [test-player-list [(tictactoe.player.EasyComputer. "o") (tictactoe.player.EasyComputer. "x")]]
+				(with-redefs [println (constantly "")]
+					(should= [(tictactoe.player.EasyComputer. "o") (tictactoe.player.EasyComputer. "x")] (with-in-str "1" (set-first-player test-player-list)))
+					(should= [(tictactoe.player.EasyComputer. "x") (tictactoe.player.EasyComputer. "o")] (with-in-str "2" (set-first-player test-player-list)))))))
 (run-specs)
--- src/tictactoe/game_setup.clj
+++ src/tictactoe/game_setup.clj
@@ -4,9 +4,9 @@
 			  [tictactoe.io :refer :all]))
 	
 (defn get-computer-difficulty [marker] 
-	(case (read-string (prompt (str "Please select computer difficulty level for computer \"" marker "\":\n  1 - Easy Computer\n  2 - Ultimate Computer")))
-		1 (tictactoe.player.EasyComputer. marker)
-		2 (tictactoe.player.UltimateComputer. marker)
+	(case (prompt (str "Please select a difficulty level for AI \"" marker "\":\n  1 - Easy Computer\n  2 - Ultimate Computer"))
+		"1" (tictactoe.player.EasyComputer. marker)
+		"2" (tictactoe.player.UltimateComputer. marker)
 		(get-computer-difficulty marker)))
 
 (defn get-player-config []	
@@ -16,6 +16,11 @@
 			"2" [(tictactoe.player.Human. "x") (tictactoe.player.Human. "o")]
 			(get-player-config)))
 
+(defn set-first-player [player-list] 
+	(case (prompt (str "Who goes first?\n  1 - " (class (first player-list)) " \"" (marker (first player-list)) "\"\n  2 - " (class (last player-list)) " \"" (marker (last player-list)) "\""))
+		"1" player-list
+		"2" [(last player-list) (first player-list)]
+		(set-first-player player-list)))
+
 (defn build-new-game []
-		[(make-board 9 nil)
-		(get-player-config)])
+	[(make-board 9 nil) (set-first-player (get-player-config))])
