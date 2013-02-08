734c1204cea93886869a07ca19f239a1a60615ff
Rylan Dirksen <rylandirksen@gmail.com>
2013-02-08 09:36:23 -0600

    Now able to select player types and AI difficulty

--- spec/tictactoe/game_setup_spec.clj
+++ spec/tictactoe/game_setup_spec.clj
@@ -4,21 +4,26 @@
             [tictactoe.game :refer :all]))
 
 (describe "game_setup"
+	; (around [nothing] (with-redefs [println (constantly "")])) ; why does this make all tests always pass?
+
 	(context "build-new-game"
-		(before-all (with-redefs [println (constantly "")]))
 		(it "should return a board and a list with two computer players"
-			(should= [[nil nil nil nil nil nil nil nil nil] [(tictactoe.player.UltimateComputer. "x") (tictactoe.player.UltimateComputer. "o")]] 
-				(with-in-str "0" (build-new-game))))
+			(should= [[nil nil nil nil nil nil nil nil nil] [(tictactoe.player.UltimateComputer. "x") (tictactoe.player.UltimateComputer. "x")]] 
+				(with-redefs [get-computer-difficulty (constantly (tictactoe.player.UltimateComputer. "x"))] 
+					(with-in-str "0" (build-new-game)))))
 		(it "should return a board and a list of one human and one computer player"
-			(should= [[nil nil nil nil nil nil nil nil nil] [(tictactoe.player.Human. "x") (tictactoe.player.UltimateComputer. "o")]] 
-				(with-in-str "1" (build-new-game))))
+			(should= [[nil nil nil nil nil nil nil nil nil] [(tictactoe.player.Human. "x") (tictactoe.player.UltimateComputer. "x")]] 
+				(with-redefs [get-computer-difficulty (constantly (tictactoe.player.UltimateComputer. "x"))] 
+					(with-in-str "1" (build-new-game)))))
 		(it "should return a board and a list with two human players"
 			(should= [[nil nil nil nil nil nil nil nil nil] [(tictactoe.player.Human. "x") (tictactoe.player.Human. "o")]] 
-				(with-in-str "2" (build-new-game)))))
+				(with-redefs [get-computer-difficulty (constantly (tictactoe.player.UltimateComputer. "x"))] 
+					(with-in-str "2" (build-new-game))))))
 	
-	(context "set-computer-difficulty"
-		(it "should" (should= (tictactoe.player.UltimateComputer. "o") (with-in-str "1" (set-computer-difficulty "o"))))
-	)
-)
+	(context "get-computer-difficulty"
+		(it "should set the computer player difficulty to easy" 
+			(should= (tictactoe.player.EasyComputer. "o") (with-in-str "1" (get-computer-difficulty "o"))))
+		(it "should set the computer player difficulty to easy" 
+			(should= (tictactoe.player.UltimateComputer. "o") (with-in-str "2" (get-computer-difficulty "o"))))))
 
 (run-specs)
--- src/tictactoe/game_setup.clj
+++ src/tictactoe/game_setup.clj
@@ -2,14 +2,20 @@
 	(:require [tictactoe.player :refer :all]
 			  [tictactoe.board :refer [make-board]]
 			  [tictactoe.io :refer :all]))
-			
+	
+(defn get-computer-difficulty [marker] 
+	(case (read-string (prompt (str "Please select computer difficulty level for computer \"" marker "\":\n  1 - Easy Computer\n  2 - Ultimate Computer")))
+		1 (tictactoe.player.EasyComputer. marker)
+		2 (tictactoe.player.UltimateComputer. marker)
+		(get-computer-difficulty marker)))
+
+(defn get-player-config []	
+	(case (prompt "How many human players?")
+			"0" [(get-computer-difficulty "x") (get-computer-difficulty "o")]
+			"1" [(tictactoe.player.Human. "x") (get-computer-difficulty "o")]
+			"2" [(tictactoe.player.Human. "x") (tictactoe.player.Human. "o")]
+			(get-player-config)))
+
 (defn build-new-game []
 		[(make-board 9 nil)
-		(case (prompt "How many human players?")
-			"0" [(tictactoe.player.UltimateComputer. "x") (tictactoe.player.UltimateComputer. "o")]
-			"1" [(tictactoe.player.Human. "x") (tictactoe.player.UltimateComputer. "o")]
-			"2" [(tictactoe.player.Human. "x") (tictactoe.player.Human. "o")])])
-
-(defn set-computer-difficulty [marker] 
-	(case (prompt "Please select computer difficulty level:\n 1 - Easy Computer\n 2 - Ultimate Computer")
-		"1" (tictactoe.player.UltimateComputer. marker)))
+		(get-player-config)])
